7591b6f39890166285dc3b238e18844f174fe6a9
YuuichiSama <142111069+YuuichiSama@users.noreply.github.com>
2024-04-09 18:13:39 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,12 +6,12 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "20389440"))
-    API_HASH = getenv("API_HASH", "a1a06a18eb9153e9dbd447cfd5da2457")
+    API_ID = int(getenv("API_ID", "18196402"))
+    API_HASH = getenv("API_HASH", "a184b91d39fc85265e232e7c323fac45")
     BOT_TOKEN = getenv("BOT_TOKEN", "6900316570:AAHKoGkozoRvILgjHgtvs0tnxWjZd5yqP50")
-    FSUB = getenv("FSUB", "VJ_Botz")
+    FSUB = getenv("FSUB", "Adult_Unity")
     CHID = int(getenv("CHID", "-1001942609289"))
-    SUDO = list(map(int, getenv("SUDO", "6168162777").split()))
+    SUDO = list(map(int, getenv("SUDO", "5911422304").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://naibansari987:Yuuichi@cluster0.wp2wkfb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
 cfg = Config()
